9530ace70fbcc88de2d34b7dde79da9ed4ea6a49
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-02-09 23:17:11 +0530

    Adding Final Application with reset option
    Clicking on Change Color sets random background color
    Clicking on Reset will set background back to black
    
    Author: shivaprasad-bhat

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
 
 const App = () => {
+  const [randomColor, setRandomColor] = useState('rgb(32, 100, 126)');
+
+  const changeBackground = () => {
+    let color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256,
+    )}, ${Math.floor(Math.random() * 256)})`;
+
+    setRandomColor(color);
+  };
+
+  const resetBackground = () => {
+    let color = 'rgb(0, 0, 0)';
+    setRandomColor(color);
+  };
   return (
     <>
-      <Text>Background Changer</Text>
+      <StatusBar backgroundColor={randomColor} />
+      <View style={[styles.container, { backgroundColor: randomColor }]}>
+        <Text style={[styles.desc]}>
+          Current background Color : {randomColor}
+        </Text>
+        <TouchableOpacity onPress={changeBackground}>
+          <Text style={styles.text}>Change Color</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={resetBackground}>
+          <Text style={styles.text}>Reset to Black</Text>
+        </TouchableOpacity>
+      </View>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 30,
+    backgroundColor: '#BB2CD9',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 15,
+    textTransform: 'uppercase',
+    marginBottom: 20,
+  },
+  desc: {
+    color: 'white',
+    fontSize: 20,
+    paddingBottom: 20,
+  },
+});
 
 export default App;
